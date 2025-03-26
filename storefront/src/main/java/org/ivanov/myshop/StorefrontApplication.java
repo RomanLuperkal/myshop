e21115c2eff6397e982f4ab275b1ee458a00274d
Roman <madara-09@bk.ru>
2025-03-27 00:34:12 +0300

    feat: подключение в проект spring data redis/cache

--- storefront/src/main/java/org/ivanov/myshop/StorefrontApplication.java
+++ storefront/src/main/java/org/ivanov/myshop/StorefrontApplication.java
@@ -2,8 +2,10 @@ package org.ivanov.myshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class StorefrontApplication {
 
     public static void main(String[] args) {
